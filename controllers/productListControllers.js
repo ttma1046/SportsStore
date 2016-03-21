35068f0266c9f15f63ebac2a2b5fbc3823f420fa
Chandler Fang <chandlerfang@gmail.com>
2016-03-22 00:10:39 +1100

    Changes

--- controllers/productListControllers.js
+++ controllers/productListControllers.js
@@ -1,36 +1,36 @@
-angular.module("sportsStore")
-.constant("productListActiveClass", "btn-primary")
-.constant("productListPageCount", 3)
-.controller("productListCtrl", function ($scope, $filter, productListActiveClass, productListPageCount, cart) {
-        
-        var selectedCategory = null;
+angular.module('sportsStore')
+.constant('productListActiveClass', 'btn-primary')
+.constant('productListPageCount', 3)
+.controller('productListCtrl', function($scope, $filter, productListActiveClass, productListPageCount, cart) {
 
-        $scope.selectedPage = 1;
-        $scope.pageSize = productListPageCount;
+  var selectedCategory = null;
 
-        $scope.selectCategory = function (newCategory) {
-            selectedCategory = newCategory;
-            $scope.selectedPage = 1;
-		};
+  $scope.selectedPage = 1;
+  $scope.pageSize = productListPageCount;
 
-        $scope.selectPage = function (newPage) {
-            $scope.selectedPage = newPage;
-        };
-        
-        $scope.categoryFilterFn = function (product) {
-            return !selectedCategory ||
-                product.category == selectedCategory;
-		};
+  $scope.selectCategory = function(newCategory) {
+    selectedCategory = newCategory;
+    $scope.selectedPage = 1;
+  };
 
-		$scope.getCategoryClass = function (category) {
-			return selectedCategory == category ? productListActiveClass : "";
-		};
+  $scope.selectPage = function(newPage) {
+    $scope.selectedPage = newPage;
+  };
 
-        $scope.getPageClass = function (page) {
-            return $scope.selectedPage == page ? productListActiveClass : "";
-        };
+  $scope.categoryFilterFn = function(product) {
+    return !selectedCategory ||
+        product.category == selectedCategory;
+  };
 
-        $scope.addProductToCart = function (product) {
-            cart.addProduct(product.id, product.name, product.price);
-        };
+  $scope.getCategoryClass = function(category) {
+    return selectedCategory == category ? productListActiveClass : '';
+  };
+
+  $scope.getPageClass = function(page) {
+    return $scope.selectedPage == page ? productListActiveClass : '';
+  };
+
+  $scope.addProductToCart = function(product) {
+    cart.addProduct(product.id, product.name, product.price);
+  };
 });
